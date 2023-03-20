a24151e76308d0eb97b738e4593ac28914fda753
bolanosfra <bolanos.dean@iisgalvanimi.edu.it>
2023-03-20 08:41:09 +0000

    punto 2

--- Verfica/src/app/app-routing.module.ts
+++ Verfica/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search-anime', component: SearchComponent },
+  { path: 'product/:id', component: ProductComponent },];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
